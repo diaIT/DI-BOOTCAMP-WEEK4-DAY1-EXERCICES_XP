5c120164301ec76919334fb2901df626b70862bb
Diallo boubacar <diallob456@gmail.com>
2023-01-14 13:49:53 +0100

    EXERCICE_XP

---
+++ Exercice_3/src/app/my-app/my-app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GreetingComponent } from '../greeting/greeting.component';
+
+
+
+@NgModule({
+  declarations: [
+    GreetingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyAppModule { }
